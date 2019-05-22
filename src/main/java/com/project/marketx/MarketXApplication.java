24585c7024cff046ce70e934f7f469405e14763b
pkrzesinski <Krzesinski.pawel@gmail.com>
2019-05-22 13:02:11 +0200

    Initial commit

---
+++ src/main/java/com/project/marketx/MarketXApplication.java
@@ -0,0 +1,13 @@
+package com.project.marketx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketXApplication.class, args);
+	}
+
+}
