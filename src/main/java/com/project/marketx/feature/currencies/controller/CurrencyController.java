d53c399e740468658b748628689a268d59e0c1c2
pkrzesinski <Krzesinski.pawel@gmail.com>
2019-05-23 14:22:39 +0200

    Exchange rate display on the main view implemented.

--- src/main/java/com/project/marketx/feature/currencies/controller/CurrencyController.java
+++ src/main/java/com/project/marketx/feature/currencies/controller/CurrencyController.java
@@ -1,6 +1,5 @@
 package com.project.marketx.feature.currencies.controller;
 
-import com.project.marketx.feature.api.AlphavantageAPI;
 import com.project.marketx.feature.api.model.CurrencyExchange;
 import com.project.marketx.feature.currencies.model.Currency;
 import com.project.marketx.feature.currencies.service.CurrencyService;
@@ -10,37 +9,44 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.ServletRequest;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("api/v1/currencies")
 public class CurrencyController {
     private static final Logger LOG = LoggerFactory.getLogger(CurrencyController.class);
+
     private CurrencyService currencyService;
-    private AlphavantageAPI alphavantageAPI;
 
     @Autowired
-    public CurrencyController(CurrencyService currencyService, AlphavantageAPI alphavantageAPI) {
+    public CurrencyController(CurrencyService currencyService) {
         this.currencyService = currencyService;
-        this.alphavantageAPI = alphavantageAPI;
     }
 
     @GetMapping("/main")
-    public String displayMainPage(Model model) {
+    public String displayMainPage(ServletRequest request, Model model) {
         List<Currency> currencyList = currencyService.getListOfCurrencies();
+
+        String fromCurrency = request.getParameter("fromCurrency");
+        String toCurrency = request.getParameter("toCurrency");
+        if (fromCurrency != null && toCurrency != null) {
+            Optional<CurrencyExchange> currencyExchange = currencyService.getCurrencyRate(fromCurrency, toCurrency);
+            currencyExchange.ifPresent(exchange -> model.addAttribute("rateModel"
+                    , exchange.getRealtimeCurrencyExchangeRate().getExchangeRate()));
+        }
 
         model.addAttribute("currencyModel", currencyList);
         return "mainView";
     }
 
-    @RequestMapping(value = "/main", method = RequestMethod.POST)
-    public void getCurrencyJson(Model model, @RequestParam("fromCurrent") String fromCurrency
-            , @RequestParam("toCurrent") String toCurrency) {
-        CurrencyExchange currencyExchange = alphavantageAPI.findExchangeRate(fromCurrency, toCurrency);
-
+    @PostMapping("/main")
+    public void getCurrencyJson(@RequestParam("fromCurrency") String fromCurrency
+            , @RequestParam("toCurrency") String toCurrency) {
     }
 }
--- src/main/java/com/project/marketx/feature/currencies/service/CurrencyService.java
+++ src/main/java/com/project/marketx/feature/currencies/service/CurrencyService.java
@@ -1,5 +1,7 @@
 package com.project.marketx.feature.currencies.service;
 
+import com.project.marketx.feature.api.AlphavantageAPI;
+import com.project.marketx.feature.api.model.CurrencyExchange;
 import com.project.marketx.feature.currencies.model.Currency;
 import com.project.marketx.jsonconverter.Converter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,16 +9,24 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class CurrencyService {
 
     private Converter converter;
+    @Autowired
+    private AlphavantageAPI alphavantageAPI;
 
     @Autowired
     public CurrencyService(Converter converter) {
         this.converter = converter;
+    }
+
+    public CurrencyService(Converter converter, AlphavantageAPI alphavantageAPI) {
+        this.converter = converter;
+        this.alphavantageAPI = alphavantageAPI;
     }
 
     public List<Currency> getListOfCurrencies() {
@@ -25,6 +35,11 @@ public class CurrencyService {
                 .stream()
                 .map(entry -> new Currency(entry.getKey(), entry.getValue()))
                 .collect(Collectors.toList());
+    }
+
+    public Optional<CurrencyExchange> getCurrencyRate(String fromCurrency, String toCurrency) {
+        CurrencyExchange currencyExchange = alphavantageAPI.findExchangeRate(fromCurrency, toCurrency);
+        return Optional.ofNullable(currencyExchange);
     }
 
     private Map<String, String> getMapOfCurrencies() {
