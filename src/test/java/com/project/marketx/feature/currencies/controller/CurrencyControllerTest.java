f0ae254a526229b9199497678569b6a7581277a6
pkrzesinski <Krzesinski.pawel@gmail.com>
2019-05-26 13:37:36 +0200

    Unity tests for CurrencyService class implemented.

--- src/test/java/com/project/marketx/feature/currencies/controller/CurrencyControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.project.marketx.feature.currencies.controller;
-
-import static org.junit.Assert.*;
-
-public class CurrencyControllerTest {
-
-}
--- src/test/java/com/project/marketx/feature/currencies/service/CurrencyServiceTest.java
+++ src/test/java/com/project/marketx/feature/currencies/service/CurrencyServiceTest.java
@@ -1,7 +1,56 @@
 package com.project.marketx.feature.currencies.service;
 
-import static org.junit.Assert.*;
+import com.project.marketx.feature.currencies.model.Currency;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.not;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class CurrencyServiceTest {
+
+    @Autowired
+    private CurrencyService sut;
+
+    @Test
+    public void shouldTestIfListOfCurrenciesIsNotNull() {
+        //given
+
+        //when
+       List<Currency> result  = sut.getListOfCurrencies();
+
+       //then
+        assertThat(result).isNotNull();
+    }
+
+    @Test
+    public void shouldTestIfListIsNotEmpty() {
+        //given
+
+        //when
+        List<Currency> result = sut.getListOfCurrencies();
+
+        //then
+        assertThat(result).isNotEmpty();
+    }
+
+    @Test
+    public void shouldTestIfListHasCorrectSize() {
+        //given
+
+        //when
+        List<Currency> result = sut.getListOfCurrencies();
+
+        //then
+        assertThat(result).hasSize(171);
+    }
+
 
 }
